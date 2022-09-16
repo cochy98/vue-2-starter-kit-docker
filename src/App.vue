6835562d50a9f9f49da5be6d2e4deb1274c6eed5
cochy98 <cosmoferrigno@gmail.com>
2022-09-16 15:17:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>My app</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
